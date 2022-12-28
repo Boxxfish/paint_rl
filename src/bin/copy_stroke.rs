aeef1e84fa6a2a833e016cec47bdd7bc203a91cb
Ben Giacalone <bensgiacalone@gmail.com>
2022-12-28 16:00:39 -0500

    Add real time plotting

--- src/bin/copy_stroke.rs
+++ src/bin/copy_stroke.rs
@@ -1,13 +1,31 @@
-/// A setup for training an agent to copy strokes from an image.
+use indicatif::ProgressIterator;
 use paint_gym::gym::{PaintAction, PaintGym, Pixel};
-
+use plotters::prelude::*;
 use rand::Rng;
 
+///
+/// A setup for training an agent to copy strokes from an image.
+///
+
+const STEPS: u32 = 10000;
 const STEPS_BEFORE_TRAINING: u32 = 200;
+const EVAL_STEPS: u32 = 100;
+const EVAL_RUNS: u32 = 4;
+const STEPS_BEFORE_EVAL: u32 = STEPS_BEFORE_TRAINING;
+const STEPS_BEFORE_PLOTTING: u32 = STEPS_BEFORE_EVAL * 10;
+const AVG_REWARD_OUTPUT: &str = "avg_rewards.png";
 
 fn main() {
-    let mut envs = PaintGym::init(4, 256, true);
-    for step in 0..1000 {
+    // Plotting stuff
+    let avg_reward_output_path = std::path::Path::new(AVG_REWARD_OUTPUT);
+    if avg_reward_output_path.exists() {
+        std::fs::remove_file(AVG_REWARD_OUTPUT).unwrap();
+    }
+    let root = BitMapBackend::new(AVG_REWARD_OUTPUT, (640, 480)).into_drawing_area();
+    let mut avg_rewards = Vec::with_capacity((STEPS / STEPS_BEFORE_PLOTTING) as usize);
+
+    let mut envs = PaintGym::init(4, 256, false);
+    for step in (0..STEPS).progress() {
         // Perform random policy
         let mut rng = rand::thread_rng();
         let mut actions = Vec::new();
@@ -23,10 +41,62 @@ fn main() {
             actions.push(PaintAction { start, end });
         }
 
-        let _results = envs.step(&actions, true);
+        let _results = envs.step(&actions, false);
 
         if (step + 1) % STEPS_BEFORE_TRAINING == 0 {
             envs.do_bg_work();
+
+            // TODO: Train the model
+        }
+
+        if (step + 1) % STEPS_BEFORE_PLOTTING == 0 {
+            // Evaluate the model's performance
+            envs.eval_mode();
+            let mut avg_rewards_now = Vec::new();
+            for _ in 0..EVAL_RUNS {
+                let mut avg_reward = 0.0;
+                for _ in 0..EVAL_STEPS {
+                    let mut actions = Vec::new();
+                    for _ in 0..envs.num_envs {
+                        let start = Pixel::new(
+                            rng.gen_range(0..envs.canvas_size),
+                            rng.gen_range(0..envs.canvas_size),
+                        );
+                        let end = Pixel::new(
+                            rng.gen_range(0..envs.canvas_size),
+                            rng.gen_range(0..envs.canvas_size),
+                        );
+                        actions.push(PaintAction { start, end });
+                    }
+                    let results = envs.step(&actions, true);
+                    avg_reward += results.results[0].2;
+                }
+                avg_rewards_now.push(avg_reward);
+            }
+            avg_rewards
+                .push(avg_rewards_now.iter().sum::<f32>() / ((EVAL_STEPS * EVAL_RUNS) as f32));
+            envs.train_mode();
+        }
+
+        if (step + 1) % STEPS_BEFORE_PLOTTING == 0 {
+            // Plot results
+            let min_reward = *avg_rewards.iter().min_by(|&&x, &y| x.total_cmp(y)).unwrap();
+            let max_reward = *avg_rewards.iter().max_by(|&&x, &y| x.total_cmp(y)).unwrap();
+            root.fill(&WHITE).unwrap();
+            let mut chart = ChartBuilder::on(&root)
+                .margin(100)
+                .x_label_area_size(30)
+                .y_label_area_size(30)
+                .build_cartesian_2d(0..avg_rewards.len(), min_reward..max_reward)
+                .unwrap();
+            chart.configure_mesh().draw().unwrap();
+            chart
+                .draw_series(LineSeries::new(
+                    avg_rewards.iter().enumerate().map(|(x, y)| (x, *y)),
+                    RED,
+                ))
+                .unwrap();
+            root.present().unwrap();
         }
     }
 
--- src/gym.rs
+++ src/gym.rs
@@ -1,4 +1,7 @@
 use minifb::{Window, WindowOptions};
+use rand::Rng;
+
+const DONE_CUTOFF: u32 = 10;
 
 /// A gym interface for a painting program.
 /// Vectorized by default.
@@ -8,7 +11,64 @@ pub struct PaintGym {
     pub canvas_size: u32,
     /// All canvases, in one contiguous array.
     canvases: Vec<(u8, u8, u8)>,
+    references: Vec<(u8, u8, u8)>,
     window: Option<Window>,
+}
+
+fn draw_reference(
+    rng: &mut rand::rngs::ThreadRng,
+    references: &mut [(u8, u8, u8)],
+    canvas_offset: usize,
+    canvas_size: u32,
+) {
+    let stroke_count = rng.gen_range(0..10);
+    for _ in 0..stroke_count {
+        let x0 = rng.gen_range(0..canvas_size) as i32;
+        let x1 = rng.gen_range(0..canvas_size) as i32;
+        let y0 = rng.gen_range(0..canvas_size) as i32;
+        let y1 = rng.gen_range(0..canvas_size) as i32;
+        plot_line(x0, x1, y0, y1, references, canvas_offset, canvas_size);
+    }
+}
+
+fn plot_line(
+    x0: i32,
+    x1: i32,
+    y0: i32,
+    y1: i32,
+    canvases: &mut [(u8, u8, u8)],
+    canvas_offset: usize,
+    canvas_size: u32,
+) {
+    // Handle straight lines
+    if x0 == x1 {
+        let start_y = y0.min(y1);
+        let end_y = y0.max(y1);
+        for y in start_y..end_y {
+            canvases[canvas_offset + y as usize * canvas_size as usize + x0 as usize] = (0, 0, 0);
+        }
+    } else if y0 == y1 {
+        let start_x = x0.min(x1);
+        let end_x = x0.max(x1);
+        for x in start_x..end_x {
+            canvases[canvas_offset + y0 as usize * canvas_size as usize + x as usize] = (0, 0, 0);
+        }
+    }
+    // Otherwise, handle sloped lines
+    else {
+        #[allow(clippy::collapsible_else_if)]
+        if (y1 - y0).abs() < (x1 - x0).abs() {
+            if x0 > x1 {
+                plot_line_low(x1, x0, y1, y0, canvases, canvas_offset, canvas_size);
+            } else {
+                plot_line_low(x0, x1, y0, y1, canvases, canvas_offset, canvas_size);
+            }
+        } else if y0 > y1 {
+            plot_line_high(x1, x0, y1, y0, canvases, canvas_offset, canvas_size);
+        } else {
+            plot_line_high(x0, x1, y0, y1, canvases, canvas_offset, canvas_size);
+        }
+    }
 }
 
 fn plot_line_low(
@@ -104,10 +164,22 @@ impl PaintGym {
         } else {
             None
         };
+
+        let mut rng = rand::thread_rng();
+        let mut references: Vec<(u8, u8, u8)> = (0..(canvas_size * canvas_size * num_envs))
+            .map(|_| (255, 255, 255))
+            .collect();
+
+        for env_id in 0..(num_envs as usize) {
+            let canvas_offset = env_id * (canvas_size * canvas_size) as usize;
+            draw_reference(&mut rng, &mut references, canvas_offset, canvas_size);
+        }
+
         Self {
             num_envs,
             canvas_size,
             canvases,
+            references,
             window,
         }
     }
@@ -121,75 +193,15 @@ impl PaintGym {
         for (env_id, action) in actions.iter().enumerate() {
             let canvas_offset = env_id * (self.canvas_size * self.canvas_size) as usize;
 
-            // Handle straight lines
-            if action.start.x == action.end.x {
-                let start_y = action.start.y.min(action.end.y);
-                let end_y = action.start.y.max(action.end.y);
-                for y in start_y..end_y {
-                    self.canvases[canvas_offset
-                        + y as usize * self.canvas_size as usize
-                        + action.start.x as usize] = (0, 0, 0);
-                }
-            } else if action.start.y == action.end.y {
-                let start_x = action.start.x.min(action.end.x);
-                let end_x = action.start.x.max(action.end.x);
-                for x in start_x..end_x {
-                    self.canvases[canvas_offset
-                        + action.start.y as usize * self.canvas_size as usize
-                        + x as usize] = (0, 0, 0);
-                }
-            }
-            // Otherwise, handle sloped lines
-            else {
-                let x0 = action.start.x as i32;
-                let x1 = action.end.x as i32;
-                let y0 = action.start.y as i32;
-                let y1 = action.end.y as i32;
-
-                if (y1 - y0).abs() < (x1 - x0).abs() {
-                    if x0 > x1 {
-                        plot_line_low(
-                            x1,
-                            x0,
-                            y1,
-                            y0,
-                            &mut self.canvases,
-                            canvas_offset,
-                            self.canvas_size,
-                        );
-                    } else {
-                        plot_line_low(
-                            x0,
-                            x1,
-                            y0,
-                            y1,
-                            &mut self.canvases,
-                            canvas_offset,
-                            self.canvas_size,
-                        );
-                    }
-                } else if y0 > y1 {
-                    plot_line_high(
-                        x1,
-                        x0,
-                        y1,
-                        y0,
-                        &mut self.canvases,
-                        canvas_offset,
-                        self.canvas_size,
-                    );
-                } else {
-                    plot_line_high(
-                        x0,
-                        x1,
-                        y0,
-                        y1,
-                        &mut self.canvases,
-                        canvas_offset,
-                        self.canvas_size,
-                    );
-                }
-            }
+            plot_line(
+                action.start.x as i32,
+                action.end.x as i32,
+                action.start.y as i32,
+                action.end.y as i32,
+                &mut self.canvases,
+                canvas_offset,
+                self.canvas_size,
+            );
         }
 
         // If we're rendering to the screen, update the window
@@ -211,11 +223,86 @@ impl PaintGym {
             }
         }
 
+        // Compute the l2 squared distance between the environment images and the reference images.
+        // We store the l2 squared distances, but return a reward of the l2 distances.
+        // This way, we can use the l2 distances as a distance metric efficiently, while keeping the
+        // reward fairly small.
+        let mut rewards = Vec::with_capacity(self.num_envs as usize);
+        let mut l2s = Vec::with_capacity(self.num_envs as usize);
+        for env_id in 0..(self.num_envs as usize) {
+            let canvas_offset = env_id * (self.canvas_size * self.canvas_size) as usize;
+            let mut l2 = 0;
+            for i in 0..(self.canvas_size * self.canvas_size) as usize {
+                let canvas_pix = self.canvases[canvas_offset + i];
+                let ref_pix = self.references[canvas_offset + i];
+                l2 += (canvas_pix.0 as i32 - ref_pix.0 as i32).pow(2) as u32
+                    + (canvas_pix.1 as i32 - ref_pix.1 as i32).pow(2) as u32
+                    + (canvas_pix.2 as i32 - ref_pix.2 as i32).pow(2) as u32;
+            }
+            rewards.push(-(l2 as f32).sqrt());
+            l2s.push(l2);
+        }
+
+        // If any environments have a lower l2 distance than the cutoff, reset that environment
+        let mut rng = rand::thread_rng();
+        for (env_id, &l2) in l2s.iter().enumerate() {
+            if l2 < DONE_CUTOFF {
+                let canvas_total = (self.canvas_size * self.canvas_size) as usize;
+                let canvas_offset = env_id * canvas_total;
+
+                // Reset the canvas
+                self.canvases.splice(
+                    canvas_offset..(canvas_offset + canvas_total),
+                    std::iter::repeat((255, 255, 255)).take(canvas_total),
+                );
+
+                // Regenerate reference
+                draw_reference(
+                    &mut rng,
+                    &mut self.references,
+                    canvas_offset,
+                    self.canvas_size,
+                );
+            }
+        }
+
         PaintStepResult {
-            results: (0..self.num_envs)
-                .map(|_| (PaintState {}, PaintState {}, 0.0))
+            results: rewards
+                .iter()
+                .map(|r| (PaintState {}, PaintState {}, *r))
                 .collect(),
         }
+    }
+
+    /// Switches the environments to use eval mode.
+    /// During this mode, only a single environment is used.
+    /// When the environment has terminated, `is_done` will be true.
+    pub fn eval_mode(&mut self) {
+        self.canvases = (0..(self.canvas_size * self.canvas_size * self.num_envs))
+            .map(|_| (255, 255, 255))
+            .collect();
+    }
+
+    /// Switches the environments to use train mode.
+    /// During this mode, multiple environments are used.
+    pub fn train_mode(&mut self) {
+        self.canvases = (0..(self.canvas_size * self.canvas_size * self.num_envs))
+            .map(|_| (255, 255, 255))
+            .collect();
+    }
+
+    /// Returns true if the environment is finished.
+    /// Only works during eval mode.
+    pub fn is_done(&self) -> bool {
+        let mut l2 = 0;
+        for i in 0..(self.canvas_size * self.canvas_size) as usize {
+            let canvas_pix = self.canvases[i];
+            let ref_pix = self.references[i];
+            l2 += (canvas_pix.0 as i32 - ref_pix.0 as i32).pow(2) as u32
+                + (canvas_pix.1 as i32 - ref_pix.1 as i32).pow(2) as u32
+                + (canvas_pix.2 as i32 - ref_pix.2 as i32).pow(2) as u32;
+        }
+        l2 < DONE_CUTOFF
     }
 
     /// Performs some work as the agent is trained.
